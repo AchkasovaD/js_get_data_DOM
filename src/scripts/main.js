669317b7deca5cef5afe0086867eb1770cbd5922
Daria Achkasova <daria.achkasova6@gmail.com>
2025-09-24 19:18:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write your code here
+const data = [...document.querySelectorAll('span.population')];
+
+const sampleText = data[0]?.textContent || '';
+const usesCommas = sampleText.includes(',');
+const usesSpaces = sampleText.includes(' ') || sampleText.includes(' ');
+
+const validNumbers = data
+  .map((span) => Number(span.textContent.replace(/,/g, '')))
+  .filter((num) => !isNaN(num));
+
+const total = validNumbers.reduce((sum, num) => sum + num, 0);
+const average = Math.round(total / validNumbers.length);
+
+function formatInSameStyle(number) {
+  if (usesCommas) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  } else if (usesSpaces) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  } else {
+    return number.toLocaleString();
+  }
+}
+
+document.querySelectorAll('span.average-population').forEach((element) => {
+  element.textContent = formatInSameStyle(average);
+});
+
+document.querySelectorAll('span.total-population').forEach((element) => {
+  element.textContent = formatInSameStyle(total);
+});
